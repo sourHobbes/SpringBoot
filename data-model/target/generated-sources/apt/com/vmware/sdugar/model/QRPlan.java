7128a8cc6e391798f6dc73ff1fead304d44b543b
Sourabh Dugar <sourabdugarsd@gmail.com>
2016-01-05 23:38:23 -0800

    add generated query DSL classes

---
+++ data-model/target/generated-sources/apt/com/vmware/sdugar/model/QRPlan.java
@@ -0,0 +1,39 @@
+package com.vmware.sdugar.model;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+
+/**
+ * QRPlan is a Querydsl query type for RPlan
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QRPlan extends EntityPathBase<RPlan> {
+
+    private static final long serialVersionUID = -578060023L;
+
+    public static final QRPlan rPlan = new QRPlan("rPlan");
+
+    public final ComparablePath<java.util.UUID> planId = createComparable("planId", java.util.UUID.class);
+
+    public final StringPath planName = createString("planName");
+
+    public QRPlan(String variable) {
+        super(RPlan.class, forVariable(variable));
+    }
+
+    public QRPlan(Path<? extends RPlan> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QRPlan(PathMetadata<?> metadata) {
+        super(RPlan.class, metadata);
+    }
+
+}
+
