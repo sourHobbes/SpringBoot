094678cb653e9d2a765f33f8359236db836c5485
Sourabh Dugar <sourabdugarsd@gmail.com>
2016-01-07 12:26:22 -0800

    add application-context.xml to do bean injection using xml. NOTE : With this method can't do app.run, the web server will not start

--- Main/src/main/java/com/vmware/sdugar/app/App.java
+++ Main/src/main/java/com/vmware/sdugar/app/App.java
@@ -3,12 +3,17 @@
  * **********************************************************************/
 package com.vmware.sdugar.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.vmware.sdugar.db.RPlanRepository;
 
 /**
  * Author: sdugar
@@ -23,7 +28,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                       "com.vmware.sdugar.model"}
 )
 public class App {
+
    public static void main(String[] args) throws Throwable {
+      /*
+      ClassPathXmlApplicationContext applicationContext =
+            new ClassPathXmlApplicationContext(new String[] {"classpath:application-context.xml"});
+      System.out.println("application context is " + applicationContext);
+      */
       SpringApplication app = new SpringApplication(App.class);
       app.setBannerMode(Banner.Mode.CONSOLE);
       app.run(args);
--- db-layer/src/main/java/com/vmware/sdugar/db/PgDbService.java
+++ db-layer/src/main/java/com/vmware/sdugar/db/PgDbService.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.google.common.collect.Lists;
@@ -26,7 +28,6 @@ import com.vmware.sdugar.model.RpVm;
  */
 @Configurable
 @Configuration
-///*not needed */@ImportResource("classpath:META-INF/data_source.xml")
 @org.springframework.transaction.annotation.Transactional(readOnly = true)
 public class PgDbService {
    @Autowired
